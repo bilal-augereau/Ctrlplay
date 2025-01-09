d265ca426a8ff8a48846af15078e0f649eb1b425
AudeNec <aude.nec@gmail.com>
2025-01-09 14:59:24 +0100

    create game seeder

--- server/database/fixtures/DeviceSeeder.ts
+++ server/database/fixtures/DeviceSeeder.ts
@@ -8,14 +8,13 @@ class DeviceSeeder extends AbstractSeeder {
   run() {
     const devices = ["PC", "PlayStation", "Xbox", "Nintendo", "Others"];
 
-    for (let i = 1; i < 6; i++) {
+    for (let i = 0; i < 5; i++) {
       const device = {
-        name: devices[i - 1],
-        refName: `device_${i}`,
+        name: devices[i],
+        refName: `device_${i + 1}`,
       };
+      this.insert(device);
     }
-
-    this.insert(devices);
   }
 }
 
