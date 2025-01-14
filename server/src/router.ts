320d872785c13f51dce9c34a840c4b95bb97df41
BILAL AUGEREAU <bilal.augereau@gmail.com>
2025-01-14 11:43:01 +0100

    addToUserLibrary

--- server/src/router.ts
+++ server/src/router.ts
@@ -7,12 +7,14 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Define item-related routes
+import gameActions from "./modules/games/gameActions";
 import itemActions from "./modules/item/itemActions";
 
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
+router.post("/api/games/users", gameActions.addToUserLibrary);
 /* ************************************************************************* */
 
 export default router;
