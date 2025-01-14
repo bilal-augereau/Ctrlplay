7ce404d13c1b121ed78aa8c131f0465e2749f226
BILAL AUGEREAU <bilal.augereau@gmail.com>
2025-01-14 17:21:37 +0100

    formatted

--- server/src/modules/games/gameRepository.ts
+++ server/src/modules/games/gameRepository.ts
@@ -3,36 +3,36 @@ import databaseClient from "../../../database/client";
 import type { Result, Rows } from "../../../database/client";
 
 type UserGame = {
-  gameId: number;
-  userId: number;
+	gameId: number;
+	userId: number;
 };
 
 class GameRepository {
-  async createUserGameLibrary(userGame: UserGame): Promise<void> {
-    const { userId, gameId } = userGame;
+	async createUserGameLibrary(userGame: UserGame): Promise<void> {
+		const { userId, gameId } = userGame;
 
-    const [user] = await databaseClient.query(
-      "SELECT id FROM user WHERE id = ?",
-      [userId],
-    );
-    if (!user) {
-      throw new Error("User not found.");
-    }
+		const [user] = await databaseClient.query(
+			"SELECT id FROM user WHERE id = ?",
+			[userId],
+		);
+		if (!user) {
+			throw new Error("User not found.");
+		}
 
-    const [game] = await databaseClient.query(
-      "SELECT id FROM game WHERE id = ?",
-      [gameId],
-    );
-    if (!game) {
-      throw new Error("Game not found.");
-    }
+		const [game] = await databaseClient.query(
+			"SELECT id FROM game WHERE id = ?",
+			[gameId],
+		);
+		if (!game) {
+			throw new Error("Game not found.");
+		}
 
-    await databaseClient.query(
-      `INSERT INTO game_shelf (user_id, game_id)
+		await databaseClient.query(
+			`INSERT INTO game_shelf (user_id, game_id)
        VALUES (?, ?)`,
-      [userId, gameId],
-    );
-  }
+			[userId, gameId],
+		);
+	}
 }
 
 export default new GameRepository();
