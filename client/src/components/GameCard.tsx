4cc18b28a4dad2dbeee0c02031aee0c61544af27
AudeNec <aude.nec@gmail.com>
2025-01-20 14:39:47 +0100

    finished user-page + rangement dossiers components

--- client/src/components/GameCard.tsx
+++ client/src/components/GameCard.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import type GameType from "../interface/GameType";
 import "./GameCard.css";
 
@@ -7,38 +6,7 @@ interface GameCardProps {
 }
 
 function GameCard({ game }: GameCardProps) {
-	const [isHovered, setIsHovered] = useState(false);
-
-	return (
-		<div
-			className={`card-game ${isHovered ? "hovered" : ""}`}
-			onMouseEnter={() => setIsHovered(true)}
-			onMouseLeave={() => setIsHovered(false)}
-		>
-			{game.image && <img src={game.image} alt={`${game.title} cover`} />}
-
-			<div className="hover-details">
-				<h3>{game.title}</h3>
-				<p>Year: {game.year ? game.year : "N/A"}</p>
-				<p>Genres: {game.genres || "Unknown"}</p>
-				<p>Devices: {game.devices?.join(", ") || "Unknown"}</p>
-				<div className="button-container">
-					<button type="button" className="beautiful-buttonadd">
-						❤️
-					</button>
-					<button type="button" className="beautiful-buttonadd">
-						⭐
-					</button>
-					<button type="button" className="beautiful-buttonadd">
-						➕
-					</button>
-					<button type="button" className="beautiful-buttonadd">
-						✅
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+	return <div>{game.title}</div>;
 }
 
 export default GameCard;
