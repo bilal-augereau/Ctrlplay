295779b7af77b4878b1a25c423872ed13575dda5
AudeNec <aude.nec@gmail.com>
2025-01-20 14:36:14 +0100

    trying to fix repo file

--- server/src/modules/gameShelf/gameShelfRepository.ts
+++ server/src/modules/gameShelf/gameShelfRepository.ts
@@ -9,12 +9,40 @@ class gameShelfRepository {
 			[userId, gameId],
 		);
 	}
+
 	async exists(userId: number, gameId: number) {
 		const [rows] = await databaseClient.query<Rows>(
 			"SELECT 1 FROM game_shelf WHERE user_id = ? AND game_id = ?",
 			[userId, gameId],
 		);
 		return rows.length > 0;
+	}
+
+	async readAllByUser(id: number) {
+		const [games] = await databaseClient.query<Rows>(
+			"SELECT * FROM game JOIN game_shelf AS gs ON gs.game_id = game.id WHERE gs.user_id = ?",
+			[id],
+		);
+
+		return games;
+	}
+
+	async readFavoritesByUser(id: number) {
+		const [games] = await databaseClient.query<Rows>(
+			"SELECT * FROM game JOIN game_shelf AS gs ON gs.game_id = game.id WHERE gs.user_id = ? AND gs.favorite = 1",
+			[id],
+		);
+
+		return games;
+	}
+
+	async readTop3TimeSpent(id: number) {
+		const [games] = await databaseClient.query<Rows>(
+			"SELECT * FROM game JOIN game_shelf AS gs ON gs.game_id = game.id WHERE gs.user_id = ? ORDER BY time_spent DESC LIMIT 3",
+			[id],
+		);
+
+		return games;
 	}
 }
 
