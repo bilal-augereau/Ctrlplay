690285e62948e5739216cfca1e90a5fa8f168265
Gustavo Cardoso <gustavo.che@live.fr>
2025-01-22 11:23:08 +0100

    cards hovering

--- client/src/components/GameCard.tsx
+++ client/src/components/GameCard.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import type GameType from "../interface/GameType";
 import "./GameCard.css";
 
@@ -7,34 +6,20 @@ interface GameCardProps {
 }
 
 function GameCard({ game }: GameCardProps) {
-  const [isHovered, setIsHovered] = useState(false);
-
   return (
-    <div
-      className={`card-game ${isHovered ? "hovered" : ""}`}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
+    <div className="card-game">
       {game.image && <img src={game.image} alt={`${game.title} cover`} />}
 
-      <div className="hover-details">
+      <div className="details">
         <h3>{game.title}</h3>
         <p>Year: {game.year ? game.year : "N/A"}</p>
         <p>Genres: {game.genres || "Unknown"}</p>
         <p>Devices: {game.devices?.join(", ") || "Unknown"}</p>
         <div className="button-container">
-          <button type="button" className="beautiful-buttonadd">
-            ❤️
-          </button>
-          <button type="button" className="beautiful-buttonadd">
-            ⭐
-          </button>
-          <button type="button" className="beautiful-buttonadd">
-            ➕
-          </button>
-          <button type="button" className="beautiful-buttonadd">
-            ✅
-          </button>
+          <button type="button">❤️</button>
+          <button type="button">⭐</button>
+          <button type="button">➕</button>
+          <button type="button">✅</button>
         </div>
       </div>
     </div>
