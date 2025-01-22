ba1756f66f27b1efc11f08bd48fe39849b935473
AudeNec <aude.nec@gmail.com>
2025-01-22 15:01:21 +0100

    correction PR

--- client/src/services/game.ts
+++ client/src/services/game.ts
@@ -1,9 +1,14 @@
+import { toast } from "react-toastify";
+
 const getGameDetails = async (id: string) => {
-	const response = await fetch(
-		`${import.meta.env.VITE_API_URL}/api/games/${id}`,
-	);
-	const data = await response.json();
-	return data;
+	try {
+		const response = await fetch(
+			`${import.meta.env.VITE_API_URL}/api/games/${id}`,
+		);
+		return await response.json();
+	} catch (error) {
+		toast.error("Error: Unable to load game details.", { theme: "dark" });
+	}
 };
 
 export default getGameDetails;
--- client/src/services/user.ts
+++ client/src/services/user.ts
@@ -1,9 +1,14 @@
+import { toast } from "react-toastify";
+
 const getUserDetails = async (id: string) => {
-	const response = await fetch(
-		`${import.meta.env.VITE_API_URL}/api/users/${id}`,
-	);
-	const data = await response.json();
-	return data;
+	try {
+		const response = await fetch(
+			`${import.meta.env.VITE_API_URL}/api/users/${id}`,
+		);
+		return await response.json();
+	} catch (error) {
+		toast.error("Error: Unable to load users infos.", { theme: "dark" });
+	}
 };
 
 export default getUserDetails;
