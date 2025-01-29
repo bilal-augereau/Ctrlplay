b630310467ccd2b90a864a7196e12e6c4ff3a64b
heyberger.augustin@gmail.com <heyberger.augustin@gmail.com>
2025-01-29 16:08:23 +0100

    finished

--- client/src/components/FavoriteButton.tsx
+++ client/src/components/FavoriteButton.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import favoriteFull from "../assets/images/button_icons/favorite.png";
 import favoriteEmpty from "../assets/images/button_icons/favorite_empty.png";
+import favoriteFull from "../assets/images/button_icons/favorite_full.png";
 
 const favoriteButton = ({
 	userId,
