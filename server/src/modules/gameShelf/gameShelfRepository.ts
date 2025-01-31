a3a3f0a8338a7705fd20131dd2cac26d3f1679c1
heyberger.augustin@gmail.com <heyberger.augustin@gmail.com>
2025-01-31 10:40:53 +0100

    US20_todolist_button

--- server/src/modules/gameShelf/gameShelfRepository.ts
+++ server/src/modules/gameShelf/gameShelfRepository.ts
@@ -57,20 +57,106 @@ class gameShelfRepository {
 		const clauses = queries.length > 0 ? `${queries.join(" ")}` : "";
 
 		const [games] = await databaseClient.query<Rows>(
-			`SELECT * FROM game JOIN game_shelf AS gs ON gs.game_id = game.id WHERE gs.user_id = ? ${clauses}`,
+			`SELECT g.*, 
+			GROUP_CONCAT(DISTINCT ge.name ORDER BY ge.name SEPARATOR ', ') AS genres, 
+			GROUP_CONCAT(DISTINCT d.name ORDER BY d.name SEPARATOR ', ') AS devices, 
+			GROUP_CONCAT(DISTINCT tag.name ORDER BY tag.name SEPARATOR ', ') AS tags, 
+			GROUP_CONCAT(DISTINCT publisher.name ORDER BY publisher.name SEPARATOR ', ') AS publishers
+		FROM game g 
+		LEFT JOIN game_genre AS gg ON gg.game_id = g.id 
+		LEFT JOIN genre ge ON gg.genre_id = ge.id 
+		LEFT JOIN game_device AS gd ON gd.game_id = g.id 
+		LEFT JOIN device d ON gd.device_id = d.id 
+		LEFT JOIN game_tag AS gt ON gt.game_id = g.id 
+		LEFT JOIN tag ON gt.tag_id = tag.id 
+		LEFT JOIN game_publisher AS gp ON gp.game_id = g.id 
+		LEFT JOIN publisher ON gp.publisher_id = publisher.id 
+		LEFT JOIN game_shelf gs ON gs.game_id = g.id 
+		WHERE gs.user_id = ? 
+		GROUP BY g.id
+        ${clauses}`,
+			values,
+		);
+		return games as GameType[];
+	}
+
+	async readFavoritesByUser(userId: number, order?: string, limit?: number) {
+		const queries = [];
+		const values: (string | number)[] = [userId];
+
+		if (order) {
+			values.push(order);
+			queries.push("ORDER BY ?");
+		}
+		if (limit) {
+			values.push(limit);
+			queries.push("LIMIT ?");
+		}
+
+		const clauses = queries.length > 0 ? `${queries.join(" ")}` : "";
+
+		const [games] = await databaseClient.query<Rows>(
+			`SELECT g.*, 
+            GROUP_CONCAT(DISTINCT ge.name ORDER BY ge.name SEPARATOR ', ') AS genres, 
+            GROUP_CONCAT(DISTINCT d.name ORDER BY d.name SEPARATOR ', ') AS devices, 
+            GROUP_CONCAT(DISTINCT tag.name ORDER BY tag.name SEPARATOR ', ') AS tags, 
+            GROUP_CONCAT(DISTINCT publisher.name ORDER BY publisher.name SEPARATOR ', ') AS publishers
+        FROM game g 
+        LEFT JOIN game_genre AS gg ON gg.game_id = g.id 
+        LEFT JOIN genre ge ON gg.genre_id = ge.id 
+        LEFT JOIN game_device AS gd ON gd.game_id = g.id 
+        LEFT JOIN device d ON gd.device_id = d.id 
+        LEFT JOIN game_tag AS gt ON gt.game_id = g.id 
+        LEFT JOIN tag ON gt.tag_id = tag.id 
+        LEFT JOIN game_publisher AS gp ON gp.game_id = g.id 
+        LEFT JOIN publisher ON gp.publisher_id = publisher.id 
+        LEFT JOIN game_shelf gs ON gs.game_id = g.id 
+        WHERE gs.user_id = ? AND gs.favorite = 1
+        GROUP BY g.id
+        ${clauses}`,
 			values,
 		);
 
 		return games as GameType[];
 	}
+	async readToDoByUser(userId: number, order?: string, limit?: number) {
+		const queries = [];
+		const values: (string | number)[] = [userId];
 
-	async readFavoritesByUser(id: number) {
+		if (order) {
+			values.push(order);
+			queries.push("ORDER BY ?");
+		}
+		if (limit) {
+			values.push(limit);
+			queries.push("LIMIT ?");
+		}
+
+		const clauses = queries.length > 0 ? `${queries.join(" ")}` : "";
+
 		const [games] = await databaseClient.query<Rows>(
-			"SELECT * FROM game JOIN game_shelf AS gs ON gs.game_id = game.id WHERE gs.user_id = ? AND gs.favorite = 1",
-			[id],
+			`SELECT g.*, 
+            GROUP_CONCAT(DISTINCT ge.name ORDER BY ge.name SEPARATOR ', ') AS genres, 
+            GROUP_CONCAT(DISTINCT d.name ORDER BY d.name SEPARATOR ', ') AS devices, 
+            GROUP_CONCAT(DISTINCT tag.name ORDER BY tag.name SEPARATOR ', ') AS tags, 
+            GROUP_CONCAT(DISTINCT publisher.name ORDER BY publisher.name SEPARATOR ', ') AS publishers
+        FROM game g 
+        LEFT JOIN game_genre AS gg ON gg.game_id = g.id 
+        LEFT JOIN genre ge ON gg.genre_id = ge.id 
+        LEFT JOIN game_device AS gd ON gd.game_id = g.id 
+        LEFT JOIN device d ON gd.device_id = d.id 
+        LEFT JOIN game_tag AS gt ON gt.game_id = g.id 
+        LEFT JOIN tag ON gt.tag_id = tag.id 
+        LEFT JOIN game_publisher AS gp ON gp.game_id = g.id 
+        LEFT JOIN publisher ON gp.publisher_id = publisher.id 
+        LEFT JOIN game_shelf gs ON gs.game_id = g.id 
+        WHERE gs.user_id = ? AND gs.to_do = 1
+        GROUP BY g.id
+        ${clauses}`,
+			values,
 		);
 
-		return games;
+		return games as GameType[];
 	}
 
 	async updateFavorite(
@@ -85,14 +171,7 @@ class gameShelfRepository {
 			[isFavorite ? 0 : 1, userId, gameId],
 		);
 	}
-	async readToDoByUser(id: number) {
-		const [games] = await databaseClient.query<Rows>(
-			"SELECT * FROM game JOIN game_shelf AS gs ON gs.game_id = game.id WHERE gs.user_id = ? AND gs.to_do = 1",
-			[id],
-		);
 
-		return games;
-	}
 	async updateToDo(
 		userId: number,
 		gameId: number,
